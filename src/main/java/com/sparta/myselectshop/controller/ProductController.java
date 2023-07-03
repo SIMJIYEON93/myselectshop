1ef893ca8da6bc617ae86139967acd5d76a76b92
SIMJIYEON93 <930515s@gmail.com>
2023-07-03 19:00:45 +0900

    관심상품 희망 최저가 업데이트 API 구현

--- src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -1,13 +1,11 @@
 package com.sparta.myselectshop.controller;
 
+import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,9 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     private final ProductService productService;
+
+
+    //관심상품 등록하기 API
     @PostMapping("/products")
     public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto){ //넘어오는 httpbody데이터 받을거
         return productService.createProduct(requestDto);
+    }
+
+    //관심상품 희망 최저가(update)등록하기 API
+    @PutMapping("/products/{id}") //pathvariavle방식
+    public ProductResponseDto updateaProducts(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto){
+        //RequestBody에서 희망데이터(ProductMypriceRequestDto) 받아
+        return productService.updateProduct(id,requestDto);
+
 
     }
 }
